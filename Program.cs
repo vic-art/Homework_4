74351022c4b03c76ee88d196d3fd4e6b236271b7
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2022-10-09 13:43:32 +0200

    Amended the solution

--- Program.cs
+++ Program.cs
@@ -5,26 +5,26 @@
 
 // 2, 4 -> 16
 
-// int A, B;
-// Console.WriteLine("Введите число А:");
-// int.TryParse(Console.ReadLine()!, out A);
+int A, B;
+Console.WriteLine("Введите число А:");
+int.TryParse(Console.ReadLine()!, out A);
 
-// Console.WriteLine("Введите число B:");
-// int.TryParse(Console.ReadLine()!, out B);
-
-
-// Console.WriteLine($"{A} в степени {B} = {Power(A, B)}");
+Console.WriteLine("Введите число B:");
+int.TryParse(Console.ReadLine()!, out B);
 
 
-// int Power(int _A, int _B)
-// {
-//     int result = 1;
-//     for (int i = 0; i < _B; i++)
-//     {
-//         result *= _A;
-//     }
-//     return result;
-// }
+Console.WriteLine($"{A} в степени {B} = {Power(A, B)}");
+
+
+int Power(int _A, int _B)
+{
+    int result = 1;
+    for (int i = 0; i < _B; i++)
+    {
+        result *= _A;
+    }
+    return result;
+}
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
@@ -33,24 +33,23 @@
 // 9012 -> 12
 // int N;
 
-// Console.WriteLine("Введите число:");
-// int.TryParse(Console.ReadLine()!, out N);
+Console.WriteLine("Введите число:");
+int.TryParse(Console.ReadLine()!, out N);
 
-// Console.WriteLine($"Сумма всех цифр в числе = {Sum(N)}");
+Console.WriteLine($"Сумма всех цифр в числе = {Sum(N)}");
 
 
-// int Sum(int _N)
-// {
-//     int result = 0;
-//     int temp = _N;
-//     while (temp != 0)
-//     {
-//         result += temp % 10;
-//         temp /= 10;
-//     }
-//     return result;
-// }
-
+int Sum(int _N)
+{
+    int result = 0;
+    int temp = _N;
+    while (temp != 0)
+    {
+        result += temp % 10;
+        temp /= 10;
+    }
+    return result;
+}
 
 
 
@@ -65,13 +64,12 @@ foreach (var item in PrintArray())
     Console.Write($"{item} ");
 }
 
-
 int[] PrintArray()
 {
     int[] _array = new int[8];
     for (int i = 0; i < _array.Length; i++)
     {
-        _array[i] = new Random().Next(11);
+        _array[i] = new Random().Next(100);
     }
     return _array;
 }
